d0a924016071f404f7dbf3580cae4b23b1c0bb3b
Andrew Ceniccola <109996166+aceniccola@users.noreply.github.com>
2025-04-04 23:17:36 -0400

    Delete node_modules directory

--- node_modules/@copilotkit/react-ui/src/components/chat/messages/UserMessage.tsx
+++
@@ -1,5 +0,0 @@
-import { UserMessageProps } from "../props";
-
-export const UserMessage = (props: UserMessageProps) => {
-  return <div className="copilotKitMessage copilotKitUserMessage">{props.message}</div>;
-};
